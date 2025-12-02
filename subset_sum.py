593a570cf4a269c1b5adcf118df8a51f5b5291ee
Vedhas <63504153+vedkharche538@users.noreply.github.com>
2025-12-02 22:56:26 +0530

    Subset sum problem

--- subset_sum.py
+++ subset_sum.py
@@ -3,20 +3,15 @@ def subset_sum(arr, arr_sum) -> bool:
     n = len(arr)
 
     # Initialization
-    t = np.zeros((n+1, arr_sum+1), dtype=bool)
-
+    t = np.zeros((n+1, arr_sum+1), dtype=int)
     for i in range(n+1):
-        t[i][0] = True
-
-    for j in range(1, arr_sum+1):
-        t[0][j] = False
-
+        t[i][0] = 1
     for i in range(1,n+1):
         for j in range(1, arr_sum + 1):
-            if j <= arr[i-1]:
-                t[i][j] = t[i-1][j] or t[i-1][j-arr[i-1]]
+            if arr[i-1] <= j:
+                t[i][j] = t[i-1][j] + t[i-1][j-arr[i-1]]
             else:
                 t[i][j] = t[i-1][j]
     return t[n][arr_sum]
 
-print(subset_sum([1,1,2,3], 4))
+print(subset_sum([1,1,1,1,1],4))
